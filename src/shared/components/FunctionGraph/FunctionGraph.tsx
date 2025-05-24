fc1087cb6b3f65b45ea07db1bffdde02ed637a4d
Roverp <yar.zubaha@proton.me>
2025-05-24 20:45:02 +0200

    feat(PageBisection): add FunctionGraph in the 4th box

--- src/shared/components/FunctionGraph/FunctionGraph.tsx
+++ src/shared/components/FunctionGraph/FunctionGraph.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useRef } from "react";
+import functionPlot from "function-plot";
+
 import "./FunctionGraph.scss";
 
-const FunctionGraph = () => {
-  return <div></div>;
+type FunctionGraphProps = {
+  fn: string;
+};
+
+const FunctionGraph = ({ fn }: FunctionGraphProps) => {
+  const graphRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!graphRef.current) return;
+
+    functionPlot({
+      target: graphRef.current,
+      width: graphRef.current.offsetWidth,
+      // FIX: cutted out numbers in top of the graph
+      height: graphRef.current.offsetHeight,
+      grid: true,
+
+      data: [
+        {
+          fn,
+        },
+      ],
+    });
+  }, []);
+
+  return (
+    <div
+      ref={graphRef}
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    ></div>
+  );
 };
 
 export default FunctionGraph;
