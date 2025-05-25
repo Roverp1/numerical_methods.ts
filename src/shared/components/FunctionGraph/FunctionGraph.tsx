07fe2034ebc8597606f291411b4c7a76df1eb8fe
Roverp <yar.zubaha@proton.me>
2025-05-26 00:33:02 +0200

    fix(FunctionGraph): fix unresponsive graph

--- src/shared/components/FunctionGraph/FunctionGraph.tsx
+++ src/shared/components/FunctionGraph/FunctionGraph.tsx
@@ -13,23 +13,23 @@ const FunctionGraph = ({ fn }: FunctionGraphProps) => {
   useEffect(() => {
     if (!graphRef.current) return;
 
-    // FIX: parse fn before drawing graph, to fix console error
     try {
       functionPlot({
         target: graphRef.current,
         width: graphRef.current.offsetWidth,
-        // FIX: cutted out numbers in top of the graph
         height: graphRef.current.offsetHeight,
         grid: true,
 
-        data: [
-          {
-            fn,
-          },
-        ],
+        data: fn
+          ? [
+              {
+                fn,
+              },
+            ]
+          : [],
       });
     } catch (err) {
-      // console.error(err);
+      console.error(err);
     }
   }, [fn]);
 
