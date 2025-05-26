e34c974938b54f34365841d4d4f9f9b9d0046ae9
Roverp <yar.zubaha@proton.me>
2025-05-26 16:06:11 +0200

    feat: install and configure vitest - a framework for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    globals: true,
+  },
 
   server: {
     port: 8888,
