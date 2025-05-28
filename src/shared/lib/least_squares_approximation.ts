e73f96794f0e39f20c123534a2a38f2f7c59b380
Roverp <yar.zubaha@proton.me>
2025-05-28 10:56:46 +0200

    refactor(least_squares_approximation): rewrite buildMatrix to more functional style

--- src/shared/lib/least_squares_approximation.ts
+++ src/shared/lib/least_squares_approximation.ts
@@ -1,17 +1,24 @@
 import type { xyPoints } from "../types";
 
 export const buildMatrix = (points: xyPoints, degree: number): number[][] => {
-  const matrix: number[][] = [];
+  // no loop mutability
+  const matrix: number[][] = Array.from({ length: degree + 1 }, (_, i) =>
+    Array.from({ length: degree + 1 }, (_, j) =>
+      points.map(([x]) => x).reduce((sum, x) => sum + x ** (i + j), 0),
+    ),
+  );
 
-  for (let i = 0; i <= degree; i++) {
-    matrix.push([]);
-
-    for (let j = 0; j <= degree; j++) {
-      const xs = points.map(([x]) => x);
-      const sum = xs.reduce((sum, x) => sum + x ** (i + j), 0);
-
-      matrix[i].push(sum);
-    }
-  }
+  // const matrix: number[][] = [];
+  //
+  // for (let i = 0; i <= degree; i++) {
+  //   matrix.push([]);
+  //
+  //   for (let j = 0; j <= degree; j++) {
+  //     const xs = points.map(([x]) => x);
+  //     const sum = xs.reduce((sum, x) => sum + x ** (i + j), 0);
+  //
+  //     matrix[i].push(sum);
+  //   }
+  // }
   return matrix;
 };
