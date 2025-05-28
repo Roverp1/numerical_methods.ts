6705ba2e911dd5dd761bdaa5da85f87294a89a07
Roverp <yar.zubaha@proton.me>
2025-05-28 11:18:43 +0200

    feat(least_squares_approximation): write tests for buildVectorB function

--- src/shared/lib/least_squares_approximation.test.ts
+++ src/shared/lib/least_squares_approximation.test.ts
@@ -1,7 +1,7 @@
 import type { xyPoints } from "../types";
-import { buildMatrix } from "./least_squares_approximation";
+import { buildMatrixA, buildVectorB } from "./least_squares_approximation";
 
-test("build matrix for 1st degree polynomial", () => {
+test("build matrixA for 1st degree polynomial", () => {
   const points: xyPoints = [
     [1, 2],
     [2, 2],
@@ -14,12 +14,12 @@ test("build matrix for 1st degree polynomial", () => {
     [10, 30],
   ];
 
-  const result = buildMatrix(points, 1);
+  const result = buildMatrixA(points, 1);
 
   expect(result).toEqual(expected);
 });
 
-test("build matrix for 2nd degree polynomial", () => {
+test("build matrixA for 2nd degree polynomial", () => {
   const points: xyPoints = [
     [-2, 1],
     [0, 2],
@@ -34,8 +34,38 @@ test("build matrix for 2nd degree polynomial", () => {
     [18, 28, 114],
   ];
 
-  const result = buildMatrix(points, 2);
-  console.log("result:", result);
+  const result = buildMatrixA(points, 2);
+
+  expect(result).toEqual(expected);
+});
+
+test("build vectorB for 1st degree polynomial", () => {
+  const points: xyPoints = [
+    [1, 1],
+    [2, 2],
+    [3, 2],
+    [4, 4],
+  ];
+
+  const expected = [9, 27];
+
+  const result = buildVectorB(points, 1);
+
+  expect(result).toEqual(expected);
+});
+
+test("build vectorB for 2nd degree polynomial", () => {
+  const points: xyPoints = [
+    [-2, 1],
+    [0, 2],
+    [1, -1],
+    [3, 1],
+    [2, 0],
+  ];
+
+  const expected = [3, 0, 12];
+
+  const result = buildVectorB(points, 2);
 
   expect(result).toEqual(expected);
 });
