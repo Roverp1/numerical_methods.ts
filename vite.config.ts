7fcea45705f4f22b94d808aa65f6e3a97fb6cfe9
MEDOYED <mmax26072005@gmail.com>
2025-05-29 20:31:59 +0200

    feat(FunctionEditor): nice UI for the formula (latex)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -13,5 +16,21 @@ export default defineConfig({
   server: {
     port: 8888,
     strictPort: true,
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      // 👉 поліфіл для "global"
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
 });
