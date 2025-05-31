4291c66eddac5f692a6912f986b28ecddadf318d
Roverp <yar.zubaha@proton.me>
2025-05-31 18:02:58 +0200

    feat: initialize PageLeastSquaresApproximation

---
+++ src/pages/PageLeastSquaresApproximation/PageLeastSquaresApproximation.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+import PointsAndFunctionGraph from "../../shared/components/PointsAndFunctionGraph/PointsAndFunctionGraph";
+import leastSquaresApproximation from "../../shared/lib/least_squares_approximation";
+
+import type { xyPoints } from "../../shared/types";
+
+import "./PageLeastSquaresApproximation.scss";
+
+type UserInput = {
+  points: xyPoints;
+  degree: number;
+};
+
+const PageLeastSquaresApproximation = () => {
+  const [userInput, setUserInput] = useState<UserInput>({
+    points: [],
+    degree: 1,
+  });
+  const [approximatedFn, setApproximatedFn] = useState<string>("");
+
+  useEffect(() => {
+    setUserInput((prev) => ({
+      points: [
+        [-2, 1],
+        [0, 2],
+        [1, -1],
+        [3, 1],
+        [2, 0],
+      ],
+      degree: prev.degree,
+    }));
+  }, []);
+
+  useEffect(() => {
+    const P = leastSquaresApproximation(userInput.points, userInput.degree);
+
+    setApproximatedFn("7-1/sqrt(x)");
+  }, [userInput]);
+
+  return (
+    <main className="page-least-squares-approximation">
+      <div className="col col-1">
+        <div className="box box-1">Input for graph points / calculator</div>
+        <div className="box box-3">Results</div>
+      </div>
+      <div className="col col-2">
+        <div className="box box-4">
+          <PointsAndFunctionGraph
+            points={userInput.points}
+            fn={approximatedFn}
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default PageLeastSquaresApproximation;
