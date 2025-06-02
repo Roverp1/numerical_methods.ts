6cc16cfd7e8f73a0c202fedb7092e60b2de59658
MEDOYED <mmax26072005@gmail.com>
2025-06-02 22:04:57 +0200

    feat(PageNewton): connect Calculator

--- src/pages/PageNewton/PageNewton.tsx
+++ src/pages/PageNewton/PageNewton.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
+import Calculator from "../../features/calculator/Calculator";
 import GraphNewton from "../../shared/components/GraphNewton/GraphNewton";
+
+import { convertLatexToExpression } from "../../shared/lib/latex/convertLatexToExpression";
 
 import "./PageNewton.scss";
 
 const PageNewton = () => {
+  const [formula, setFormula] = useState<string>("");
+
+  const onChangeLatex = (latex: string) => {
+    const parsed = convertLatexToExpression(latex);
+    setFormula(parsed);
+  };
+
   return (
     <>
       <main className="page-newton">
         <div className="col col-1">
-          <div className="box box-1">test1</div>
+          <div className="box box-1">
+            <Calculator onChangeLatex={onChangeLatex} />
+          </div>
           <div className="box box-3">test3</div>
         </div>
         <div className="col col-2">
