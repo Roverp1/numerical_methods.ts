cba8f56f2a1b0150398c015cec8890598e14b57d
Roverp <yar.zubaha@proton.me>
2025-06-06 13:27:51 +0200

    feat: start writing bisectionMethod using functional programming paradigms

---
+++ src/shared/lib/newton_method.ts
@@ -0,0 +1,27 @@
+type Fn = (x: number) => number;
+
+// (f(x + h) - f(x - h)) / (2 * h)
+const symmetricDifferenceQuotient = (fn: Fn, h: number = 1e-5): Fn => {
+  return (x: number) => (fn(x + h) - fn(x - h)) / (2 * h);
+};
+
+const newtonMethod = (
+  fn: Fn,
+  xp: number,
+  tolerance: number = 1e-6,
+  maxIter?: number,
+): number => {
+  const dfn = symmetricDifferenceQuotient(fn);
+  let xc: number = xp;
+  const limit = maxIter || 1000;
+
+  for (let i = 0; i < limit; i++) {
+    const xn = xc - fn(xc) / dfn(xc);
+
+    if (Math.abs(xn - xc) < tolerance) return xn;
+
+    xc = xn;
+  }
+  console.error("Root not found");
+  return NaN;
+};
