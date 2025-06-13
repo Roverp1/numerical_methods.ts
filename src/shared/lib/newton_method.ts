7a589c392cd0e4b6fd0d25b7f0d94a7c277ffe73
Roverp <yar.zubaha@proton.me>
2025-06-13 14:46:16 +0200

    refactor(newton_method): make code more functinal

--- src/shared/lib/newton_method.ts
+++ src/shared/lib/newton_method.ts
@@ -9,19 +9,29 @@ const newtonMethod = (
   fn: Fn,
   xp: number,
   tolerance: number = 1e-6,
-  maxIter?: number,
+  maxIter: number = 1000,
 ): number => {
   const dfn = symmetricDifferenceQuotient(fn);
-  let xc: number = xp;
-  const limit = maxIter || 1000;
 
-  for (let i = 0; i < limit; i++) {
+  const recursionHelper = (xc: number, i: number = 0) => {
+    if (i >= maxIter) return xc;
+
     const xn = xc - fn(xc) / dfn(xc);
 
     if (Math.abs(xn - xc) < tolerance) return xn;
 
-    xc = xn;
-  }
+    return recursionHelper(xn, i + 1);
+  };
+
+  return recursionHelper(xp);
+
+  // for (let i = 0; i < limit; i++) {
+  //   const xn = xc - fn(xc) / dfn(xc);
+  //
+  //   if (Math.abs(xn - xc) < tolerance) return xn;
+  //
+  //   xc = xn;
+  // }
 
   console.error("Root not found");
   return NaN;
